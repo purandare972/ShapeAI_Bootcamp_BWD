d6482b363ab0bcc372521c291ffff2ec1c70d95d
purandare972 <55679792+purandare972@users.noreply.github.com>
2021-06-28 00:28:44 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js</h1>
+      <p>
+        A basic web dev Bootcamp Using HTML,CSS and Js Taken By Shaurya Sinha
+      </p>
+    </div>
+  );
+}
+
+export default Info;
